306be13bd0efde29ef143d6a5b05910c004ba379
Clement Nkamanyi <c.nkamanyi@clenkasoft.com>
2024-03-04 04:01:09 +0100

    corrected error with example

--- AzureWebapp.tf
+++ AzureWebapp.tf
@@ -3,8 +3,8 @@ resource "azurerm_resource_group" "clemic-demorg" {
   location = "West Europe"
 }
 
-resource "azurerm_app_service_plan" "example" {
-  name                = "example-appserviceplan"
+resource "azurerm_app_service_plan" "clemic-demorg-applan" {
+  name                = "clemic-demorg-applan"
   location            = azurerm_resource_group.clemic-demorg.location
   resource_group_name = azurerm_resource_group.clemic-demorg.name
 
@@ -14,11 +14,11 @@ resource "azurerm_app_service_plan" "example" {
   }
 }
 
-resource "azurerm_app_service" "clemic-demo-splan" {
-  name                = "clemic-demo-splan"
+resource "azurerm_app_service" "clemic-demo-app" {
+  name                = "clemic-demo-app"
   location            = azurerm_resource_group.clemic-demorg.location
   resource_group_name = azurerm_resource_group.clemic-demorg.name
-  app_service_plan_id = azurerm_app_service_plan.clemic-demo-splan.id
+  app_service_plan_id = azurerm_app_service_plan.clemic-demorg-applan.id
 
   site_config {
     dotnet_framework_version = "v4.0"
